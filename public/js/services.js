a0453d9975ece56010eb0b7c2edc1d1c8df722dd
Kirill Antonov <chapluck@gmail.com>
2017-09-11 22:47:48 +0300

    Set 2 sources: rss and FGA. Use rss2json service

--- public/js/services.js
+++ public/js/services.js
@@ -3,89 +3,81 @@
 /* Services */
 
 angular.module('RandomTipServices', ['ngSanitize', 'ngResource'])
-    .factory('TipProvider', ['$sce', '$timeout', 'FGAdviceProvider', 'RssProvider', 'ReadabilityProvider', function($sce, $timeout, FGAdviceProvider, RssProvider, ReadabilityProvider){
-    	var service = {
-    	      currentTip: {
-    	        title: 'Loading...',
-    	        content: $sce.trustAsHtml('<h2>New tip is being loaded</h2>')
-    	      }
-    	};
-    
-    	function run(){
-            var picker = Math.floor(Math.random()*3);
-            if(picker < 2){
+    .factory('TipProvider', ['$sce', '$timeout', 'FGAdviceProvider', 'RssProvider', 'ReadabilityProvider', function ($sce, $timeout, FGAdviceProvider, RssProvider, ReadabilityProvider) {
+        var service = {
+            currentTip: {
+                title: 'Loading...',
+                content: $sce.trustAsHtml('<h2>New tip is being loaded</h2>')
+            }
+        };
+
+        function run() {
+            var picker = Math.floor(Math.random() * 3);
+            if (picker < 2) {
                 var provider = FGAdviceProvider;
             }
-            else{
+            else {
                 var provider = RssProvider;
             }
-            provider = ReadabilityProvider;
-            provider.getTip(function(tip){
-                service.currentTip.title = $sce.trustAsHtml(tip.title);
-       	        service.currentTip.content = $sce.trustAsHtml(tip.content);
-            	$timeout(function(){run();}, 1 * 600000);
+            //provider = ReadabilityProvider;
+            provider.getTip(function (tip) {
+                if(tip){
+                    service.currentTip.title = $sce.trustAsHtml(tip.title);
+                    service.currentTip.content = $sce.trustAsHtml(tip.content);
+                    $timeout(function () { run(); }, 1 * 600000);
+                }
+                else
+                    $timeout(function () { run(); }, 0);                
             });
-    	}
-    
-    	$timeout(function(){run();}, 5000);
-      return service;
+        }
+
+        $timeout(function () { run(); }, 5000);
+        return service;
     }])
-    .factory('FGAdviceProvider', ['$resource', function($resource){
+    .factory('FGAdviceProvider', ['$resource', function ($resource) {
         var service = $resource('/external/fga', {}, {
             fetch: { method: 'GET', isArray: false }
         });
         return {
-           getTip: function(setTip){
+            getTip: function (setTip) {
                 service.fetch(function (data) { //lookup title
-                        setTip({title: "&nbsp;-&nbsp;" + data.text, content: ""});
-                    });
-           }
+                    setTip({ title: "&nbsp;-&nbsp;" + data.text, content: "" });
+                });
+            }
         };
     }])
-    .factory('ReadabilityProvider', ['$resource', function($resource){
+    .factory('ReadabilityProvider', ['$resource', function ($resource) {
         var service = $resource('/external/readability', {}, {
             fetch: { method: 'GET', isArray: false }
         });
         return {
-           getTip: function(setTip){
+            getTip: function (setTip) {
                 service.fetch(function (data) { //lookup title
-                        setTip({title: "Readability" + data.text, content: ""});
-                    });
-           }
+                    setTip({ title: "Readability" + data.text, content: "" });
+                });
+            }
         };
     }])
-    .factory('RssProvider', ['UrlLookup', 'FeedLoad', function(UrlLookup, FeedLoad){
+    .factory('RssProvider', ['FeedLoader', function (FeedLoader) {
         return {
-           getTip: function(setTip){
-                UrlLookup.fetch({q: 'http://www.i-programmer.info/index.php?option=com_ninjarsssyndicator&feed_id=1&format=raw'}, {}, function (data) {
-                    if (data.responseStatus != 200 || (data.responseData && data.responseData.url == '')) {
-                        alert(data.responseDetails || 'Feed not found!');
-                        return;
-                    }
-        
-                    var feed = data.responseData;
-                    FeedLoad.fetch({q: data.responseData.url}, {}, function (data) { //lookup title
-                        if (data.responseStatus != 200) {
-                            return;
+            getTip: function (setTip) {
+                FeedLoader.fetch({ rss_url: 'http://www.i-programmer.info/index.php?option=com_ninjarsssyndicator&feed_id=1&format=raw' }, {},
+                    function (data) { //lookup title
+                        if (data.status != "ok" || !data.items.length) {
+                            return setTip(null);
                         }
-
-            		    setTip({
-                            title: data.responseData.feed.entries[0].title, 
-                            content: data.responseData.feed.entries[0].content
+                        var picker = Math.floor(Math.random() * data.items.length);                        
+                        setTip({
+                            title: data.items[picker].title,
+                            content: data.items[picker].content
                         });
                     });
-                });
-           }
+            }
         }
     }])
-    .factory('FeedLoad', function ($resource) {
-        return $resource('http://ajax.googleapis.com/ajax/services/feed/load', {}, {
-            fetch: { method: 'JSONP', params: {v: '1.0', callback: 'JSON_CALLBACK'} }
-        });
-    })
-    .factory('UrlLookup', function ($resource) {
-        return $resource('http://ajax.googleapis.com/ajax/services/feed/lookup', {}, {
-            fetch: { method: 'JSONP', params: {v: '1.0', callback: 'JSON_CALLBACK'} }
+    .factory('FeedLoader', function ($resource) {
+        return $resource('https://api.rss2json.com/v1/api.json', {}, {
+            fetch: { method: 'GET', params: {} }
         });
     })
     .service('LocalObjectStorage', function () {
@@ -111,12 +103,12 @@ angular.module('RandomTipServices', ['ngSanitize', 'ngResource'])
             var id = localStorage.getItem('FeedListId') ? localStorage.getItem('FeedListId') : 1;
 
             list.push({
-                url:    url,
-                title:  title,
+                url: url,
+                title: title,
                 id: id
             });
 
-            LocalObjectStorage.setObject('FeedList',  list);
+            LocalObjectStorage.setObject('FeedList', list);
             localStorage.setItem('FeedListId', ++id);
             $rootScope.$broadcast('FeedList', list);
         };
@@ -146,7 +138,7 @@ angular.module('RandomTipServices', ['ngSanitize', 'ngResource'])
             });
         };
 
-        this.getById = function(id) {
+        this.getById = function (id) {
             var list = this.get();
 
             for (var i = list.length - 1; i >= 0; i--) {
